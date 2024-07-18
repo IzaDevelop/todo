c06fb40d7626e89a53749c7bc87865f3ceaca0d3
Izabelle <izabelledevelop@gmail.com>
2024-07-17 23:03:35 -0300

    todo in standalone

---
+++ src/app/components/add-task/add-task.component.ts
@@ -0,0 +1,44 @@
+import { CommonModule } from '@angular/common';
+import { Component, Output, EventEmitter } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { Tarefa } from '../../../tarefa';
+import { ButtonComponent } from '../button/button.component';
+
+@Component({
+  selector: 'app-add-task',
+  standalone: true,
+  imports: [CommonModule, FormsModule, ButtonComponent],
+  templateUrl: './add-task.component.html',
+  styleUrl: './add-task.component.css'
+})
+export class AddTaskComponent {
+  @Output() onAddTask = new EventEmitter<Tarefa>();
+
+  tarefa: string = '';
+  categoria: string = '';
+  concluido: boolean = false;
+  mostrarAddTarefa: boolean = false;
+
+  onSubmit(){
+    if (!this.tarefa) {
+      alert('Adicione uma tarefa')
+      return
+    }
+
+    const novaTarefa = {
+      tarefa: this.tarefa,
+      categoria: this.categoria,
+      concluido: this.concluido
+    }
+
+    this.onAddTask.emit(novaTarefa)
+
+    this.tarefa = '',
+    this.categoria = '',
+    this.concluido = false
+  }
+
+  AlteraVisualizacao(valor: boolean){
+    this.mostrarAddTarefa = valor;
+  }
+}
